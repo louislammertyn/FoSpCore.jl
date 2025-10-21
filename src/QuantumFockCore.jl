f1f401e90453e13187d74b8a36ac9df2060f9240
Louis Lammertyn <louis.lammertyn6@gmail.com>
2025-10-21 10:09:38 +0200

    minor bugfix

--- src/QuantumFockCore.jl
+++ src/QuantumFockCore.jl
@@ -21,7 +21,7 @@ export AbstractFockSpace, U1FockSpace, UnrestrictedFockSpace,
 export fock_state, copy, cleanup_FS, checkU1, basisFS
 export a_j, ad_j
 export norm2FS
-export all_states_U1, create_MFS, dot
+export  create_MFS, dot
 
 export MutableFockState, to_fock_state, reset!, reset2!, norm2FS, cleanup_FS, mul_Mutable!
 export a_j!, ad_j!
@@ -31,12 +31,10 @@ export a_j!, ad_j!
 
 
 export AbstractFockOperator, FockOperator, MultipleFockOperator, ZeroFockOperator, identity_fockoperator
-export calculate_matrix_elements,  calculate_matrix_elements_naive, calculate_matrix_elements_parallel, tuple_vector_equal
-export sparseness
 export cleanup_FO, dagger_FO
 export apply!, apply
-export rand_superpos, diagonalise_KR
-export all_states_U1_O
+export rand_superpos
+
 
 #####################################################################################################
 #####################################################################################################
